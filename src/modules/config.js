a47c7bacfced746dc2b34d46c6f1ab119eb50ece
Matthew James Davis <davis.matthewjames@gmail.com>
2016-02-18 16:05:48 -0500

    add config / d.ts upgrade to typings

---
+++ src/modules/config.js
@@ -0,0 +1,12 @@
+System.register([], function(exports_1) {
+    "use strict";
+    var config;
+    return {
+        setters:[],
+        execute: function() {
+            exports_1("config", config = {
+                debug: true
+            });
+        }
+    }
+});
